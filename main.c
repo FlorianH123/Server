311bbdb5cee6f50f4cc44428c73c85365d3c8434
FlorianH123 <f.hennrich@gmx.de>
2017-11-30 17:00:49 +0100

    worker

--- main.c
+++ main.c
@@ -11,20 +11,17 @@
 #define STANDARD_PORT 5193
 
 /*--- Protofunktionen --- */
-void readFileAndSendData(char*);
+void readFileAndSendData(SOCKET);
 
 /*--- Ende Protofunktionen ---*/
 
 /*--- Konstanten ---*/
-const char* usage = "Geben Sie die Anzahl der ersten n Bytes (-<n Bytes>) und max. 5 Dateinamen (mit -) an,"
-        " die Sie erhalten wollen\n";
-
 const char* fileNotFoundException = "Die Datei wurde nicht gefunden!\n";
 /*--- Ende Konstanten ---*/
 
-void readFileAndSendData(char* buffer1) {
+void readFileAndSendData(SOCKET workerSocketDescriptor) {
     const int buffer_len = 250;
-    char* buffer = buffer1;
+    char* buffer = NULL;
 
     char* arg;
     char delimiter[] = "-, ";
@@ -32,8 +29,7 @@ void readFileAndSendData(char* buffer1) {
     FILE *fp;
 
     //memset(buffer, '\0', sizeof(buffer));
-    //send(workerSocketDescriptor, usage, strlen(usage), 0);
-    //recv(workerSocketDescriptor, buffer, buffer_len, 0);
+    recv(workerSocketDescriptor, buffer, buffer_len, 0);
 
     nBytes = atoi(strtok(buffer, delimiter));
     arg = strtok(NULL, delimiter);
@@ -43,7 +39,7 @@ void readFileAndSendData(char* buffer1) {
         char charArray[nBytes + 1];
 
         if (fp == NULL) {
-            //send(workerSocketDescriptor, fileNotFoundException, strlen(fileNotFoundException), 0);
+            send(workerSocketDescriptor, fileNotFoundException, strlen(fileNotFoundException), 0);
             fprintf(stderr, "Datei %s nicht gefunden\n", arg);
         } else {
             while (n < nBytes && (c = fgetc(fp)) != EOF) {
@@ -52,7 +48,7 @@ void readFileAndSendData(char* buffer1) {
             }
 
             charArray[n] = '\0';
-            //send(workerSocketDescriptor, (const char *) charArray, nBytes + 1, 0);
+            send(workerSocketDescriptor, (const char *) charArray, nBytes + 1, 0);
 
             fclose(fp);
             n = 0;
@@ -62,51 +58,47 @@ void readFileAndSendData(char* buffer1) {
     }
 }
 
-//int main() {
-//    int port = STANDARD_PORT; /* Protokoll-Portnummer */
-//
-//    struct sockaddr_in serveraddr;  /* Struktur fuer die Serveradresse */
-//    struct sockaddr_in clientaddr;  /* Struktur fuer die Clientadresse */
-//
-//    serveraddr.sin_family = AF_INET;             /* Familie auf Internet setzen */
-//    serveraddr.sin_port = htons((u_short) port);  /* host-byte-order -> network byte-order */
-//    serveraddr.sin_addr.s_addr = INADDR_ANY;     /* Lokale IP-Adressen setzen */
-//
-//    SOCKET workerSocketDescriptor;
-//    SOCKET serverSocketDescriptor;
-//
-//    /* Socket erzeugen */
-//    serverSocketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
-//
-//    if (serverSocketDescriptor < 0) {
-//        fprintf(stderr, "socket creation failed\n");
-//        exit(3);
-//    }
-//
-//    /* Lokale Adresse mit Socket binden */
-//    if (bind(serverSocketDescriptor, (struct sockaddr*) &serveraddr, sizeof(serveraddr)) < 0) {
-//        fprintf(stderr, "bind failed\n");
-//        exit(4);
-//    }
-//
-//    /* Groesse der Anfragewarteschlange bestimmen */
-//    if (listen(serverSocketDescriptor, QUEUELENGTH) < 0) {
-//        fprintf(stderr, "listen failed\n");
-//        exit(5);
-//    }
-//
-//    while(1) {
-//        if ((workerSocketDescriptor = accept(serverSocketDescriptor, (struct sockaddr *)&clientaddr,
-//                                             (int *) sizeof clientaddr)) < 0) {
-//            fprintf(stderr, "accept failed\n");
-//            exit(6);
-//        }
-//
-//        readFileAndSendData(workerSocketDescriptor);
-//    }
-//}
+int main() {
+    int port = STANDARD_PORT; /* Protokoll-Portnummer */
 
-int main () {
-    char a[] = "-20 -dateiA.txt -dateiB.txt - dateiC.txt";
-    readFileAndSendData(a);
+    struct sockaddr_in serveraddr;  /* Struktur fuer die Serveradresse */
+    struct sockaddr_in clientaddr;  /* Struktur fuer die Clientadresse */
+
+    serveraddr.sin_family = AF_INET;             /* Familie auf Internet setzen */
+    serveraddr.sin_port = htons((u_short) port);  /* host-byte-order -> network byte-order */
+    serveraddr.sin_addr.s_addr = INADDR_ANY;     /* Lokale IP-Adressen setzen */
+
+    SOCKET workerSocketDescriptor;
+    SOCKET serverSocketDescriptor;
+
+    /* Socket erzeugen */
+    serverSocketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serverSocketDescriptor < 0) {
+        fprintf(stderr, "socket creation failed\n");
+        exit(3);
+    }
+
+    /* Lokale Adresse mit Socket binden */
+    if (bind(serverSocketDescriptor, (struct sockaddr*) &serveraddr, sizeof(serveraddr)) < 0) {
+        fprintf(stderr, "bind failed\n");
+        exit(4);
+    }
+
+    /* Groesse der Anfragewarteschlange bestimmen */
+    if (listen(serverSocketDescriptor, QUEUELENGTH) < 0) {
+        fprintf(stderr, "listen failed\n");
+        exit(5);
+    }
+
+    while(1) {
+        if ((workerSocketDescriptor = accept(serverSocketDescriptor, (struct sockaddr *)&clientaddr,
+                                             (int *) sizeof clientaddr)) < 0) {
+            fprintf(stderr, "accept failed\n");
+            exit(6);
+        }
+
+        readFileAndSendData(workerSocketDescriptor);
+    }
 }
+
